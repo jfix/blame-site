28f4a418fda6e42dc0f661e05f8e09ca676de544
Jakob Fix <jakob.fix@gmail.com>
2022-09-11 11:49:14 +0200

    Diverse changes to make it work again

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require('dotenv').config()
 const express = require('express')
-const giphy = require('giphy-api')()
+const giphy = require('giphy-api')(process.env.GIPHY_API_KEY)
 const path = require('path')
 
 const phrases = [
   'Blame on you!!!',
   'Consider yourself blamed!!!',
-  'You\'ve been blamed right now!!!'
+  'You\'ve been blamed right now!!!',
+  'Such blame! Wow!'
 ]
 const keywords = [
-  'rage', 'leave', 'get-out', 'stop-it'
+  'rage', 'leave', 'get-out', 'stop-it', 'blame'
 ]
 
 const app = express()
@@ -19,14 +20,16 @@ app.use(express.static(path.join(__dirname, 'assets')))
 app.get('/', (req, res) => {
   giphy.random(keywords[Math.floor(Math.random() * keywords.length)])
   .then((gif) => {
+    console.log(`gif is: ${gif.data.images.original.url}`)
     res.render('index', {
       title: 'Blam-blam-blame!',
       message: phrases[Math.floor(Math.random() * phrases.length)],
-      image: gif.data.image_url })
+      image: gif.data.images.original.url
+    })
   })
   .catch((err) => {
     res.send(JSON.stringify(err))
   })
 })
-
-app.listen(process.env.PORT || 3000, () => console.log('Blamer now running!'))
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Blamer now running on ${port}!`))
